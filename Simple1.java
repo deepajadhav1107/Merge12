28613fc6b76f24bb4c8838edf367da30f5da3105
deepajadhav1107 <deepajadhav2001@gmail.com>
2025-04-16 12:51:13 +0530

    save

---
+++ Simple1.java
@@ -0,0 +1,9 @@
+public class Simple1 {
+    public static void main(String args[])
+        {
+         int x=10;
+         int y=20;
+         int sub=x-y;
+         System.out.println("SUB is"+sub);   
+        }
+}
