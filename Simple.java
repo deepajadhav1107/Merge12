bdebb2b38328267b36b6de7764b49b1a6c762950
deepajadhav1107 <deepajadhav2001@gmail.com>
2025-04-16 12:47:19 +0530

    save

---
+++ Simple.java
@@ -0,0 +1,12 @@
+public class Simple {
+
+        public static void main(String args[])
+        {
+         int x=10;
+         int y=20;
+         int sub=x-y;
+         System.out.println("SUM is"+sub);   
+         System.out.println("main");
+        }
+    
+}
